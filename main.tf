be66b8ded51db1fcd348a1f51756d87d7a73d14d
sravanireddyyagnith <148060883+sravanireddyyagnith@users.noreply.github.com>
2023-11-23 21:58:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 resource "aws_iam_user" "Admin" {
     name = "sravani2"
     tags = {
